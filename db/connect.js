9a14cedfb156be4998e2133a7926154e6aa57eff
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-01-25 19:17:57 +0100

    Lesson-3

---
+++ db/connect.js
@@ -0,0 +1,32 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const MongoClient = require('mongodb').MongoClient;
+
+let _db;
+
+const initDb = (callback) => {
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+  MongoClient.connect('mongodb+srv://dbadmin:dbadmin@cluster0.ueq2x2a.mongodb.net/?retryWrites=true&w=majority')
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
+
+const getDb = () => {
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
+};
+
+module.exports = {
+  initDb,
+  getDb
+};
